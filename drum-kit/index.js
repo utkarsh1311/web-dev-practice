24245941d82dbbfa9ce4de39381db893226321cc
Utkarsh <itsutkarsh123@gmail.com>
2021-09-17 21:11:53 +0530

    added listeners for detecting keyboard strokes to buttons

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,50 +1,62 @@
 var numberOfDrum = document.querySelectorAll(".drum-part").length;
 
+// Detecting button press
+
 for (let index = 0; index < numberOfDrum; index++) {
 	document
 		.querySelectorAll(".drum-part")
 		[index].addEventListener("click", function () {
 			var buttonInnerHtml = this.innerHTML;
-            console.log(buttonInnerHtml);
-			switch (buttonInnerHtml) {
-				case "w":
-					var wAudio = new Audio("sounds/crash.mp3");
-					wAudio.play();
-					break;
-
-				case "q":
-					var qAudio = new Audio("sounds/kick-bass.mp3");
-					qAudio.play();
-					break;
-
-				case "a":
-					var aAudio = new Audio("sounds/snare.mp3");
-					aAudio.play();
-					break;
-
-				case "s":
-					var sAudio = new Audio("sounds/tom-1.mp3");
-					sAudio.play();
-					break;
-
-				case "j":
-					var jAudio = new Audio("sounds/tom-2.mp3");
-					jAudio.play();
-					break;
-
-				case "k":
-					var kAudio = new Audio("sounds/tom-3.mp3");
-					kAudio.play();
-					break;
-
-				case "l":
-					var lAudio = new Audio("sounds/tom-4.mp3");
-					lAudio.play();
-					break;
-
-				default:
-                    alert("Not a valid case");
-					break;
-			}
+			playSoundByKeyPress(buttonInnerHtml);
 		});
+}
+
+
+// For detecting keyboard press 
+
+document.addEventListener("keydown", function(e) {
+    playSoundByKeyPress(e.key);
+})
+
+function playSoundByKeyPress(key) {
+	switch (key) {
+		case "w":
+			var wAudio = new Audio("sounds/crash.mp3");
+			wAudio.play();
+			break;
+
+		case "q":
+			var qAudio = new Audio("sounds/kick-bass.mp3");
+			qAudio.play();
+			break;
+
+		case "a":
+			var aAudio = new Audio("sounds/snare.mp3");
+			aAudio.play();
+			break;
+
+		case "s":
+			var sAudio = new Audio("sounds/tom-1.mp3");
+			sAudio.play();
+			break;
+
+		case "j":
+			var jAudio = new Audio("sounds/tom-2.mp3");
+			jAudio.play();
+			break;
+
+		case "k":
+			var kAudio = new Audio("sounds/tom-3.mp3");
+			kAudio.play();
+			break;
+
+		case "l":
+			var lAudio = new Audio("sounds/tom-4.mp3");
+			lAudio.play();
+			break;
+
+		default:
+			console.log("Not valid keystroke");
+			break;
+	}
 }
