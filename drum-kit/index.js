b605e02c1b0b2d995c0f30b73f18b165df730f65
Utkarsh <itsutkarsh123@gmail.com>
2021-09-17 20:10:16 +0530

    added some event listener to buttons to play music

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,1 +1,7 @@
-alert("This is connected");
+
+document.querySelectorAll(".drum-part").forEach((element) => {
+	element.addEventListener("click", function () {
+		var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+	});
+});
