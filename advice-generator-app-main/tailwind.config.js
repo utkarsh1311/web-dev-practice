7bd535f54e68752be5c5c1a41e05842cd8b5020a
Utkarsh <itsutkarsh123@gmail.com>
2022-04-06 00:01:52 +0530

    completed styling for mobile

--- advice-generator-app-main/tailwind.config.js
+++ advice-generator-app-main/tailwind.config.js
@@ -3,15 +3,18 @@ module.exports = {
 	theme: {
 		extend: {},
 		colors: {
-			lcyan : "hsl(193, 38%, 86%)",
+			lcyan: "hsl(193, 38%, 86%)",
 			ngreen: "hsl(150, 100%, 66%)",
 			gblue: "hsl(217, 19%, 38%)",
 			dgblue: "hsl(217, 19%, 24%)",
-			dblue: "hsl(218, 23%, 16%)"
+			dblue: "hsl(218, 23%, 16%)",
 		},
 		fontFamily: {
-			'manrope': ['Spartan', 'sans-serif'],
-		  },
+			manrope: ["Spartan", "sans-serif"],
+		},
+		fontSize: {
+			'quote': '28px',
+		},
 	},
 	plugins: [],
 };
