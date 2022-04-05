7332a95b260f6835dd6f1d5e6dbeaaf0eeddb4e1
Utkarsh <itsutkarsh123@gmail.com>
2022-04-05 22:47:05 +0530

    add basic styling

--- advice-generator-app-main/tailwind.config.js
+++ advice-generator-app-main/tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
 	content: ["./*.{html,js}"],
 	theme: {
 		extend: {},
+		colors: {
+			lcyan : "hsl(193, 38%, 86%)",
+			ngreen: "hsl(150, 100%, 66%)",
+			gblue: "hsl(217, 19%, 38%)",
+			dgblue: "hsl(217, 19%, 24%)",
+			dblue: "hsl(218, 23%, 16%)"
+		},
+		fontFamily: {
+			'manrope': ['Spartan', 'sans-serif'],
+		  },
 	},
 	plugins: [],
 };
