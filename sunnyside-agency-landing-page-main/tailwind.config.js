9255130a8b7bb695e0e14bdd74d17a663d41e7cb
Utkarsh <itsutkarsh123@gmail.com>
2022-04-13 13:34:30 +0530

    complete first section

--- sunnyside-agency-landing-page-main/tailwind.config.js
+++ sunnyside-agency-landing-page-main/tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: ["*.{html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["*.html"],
+	theme: {
+		extend: {
+			colors: {
+				"grey-blue-light": "hsl(210, 4%, 67%)",
+				"dark-de-cyan": "hsl(167, 40%, 24%)",
+				"dark-blue": "hsl(198, 62%, 26%)",
+				"soft-red": "hsl(7, 99%, 70%)",
+				yellow: "hsl(51, 100%, 49%)",
+				"dark-mod-cyan": "hsl(168, 34%, 41%)",
+				"vdark-de-blue": "hsl(212, 27%, 19%)",
+				"very-dark-grey-blue": "hsl(213, 9%, 39%)",
+				"dark-grey-blue": "hsl(232, 10%, 55%)",
+				"grey-blue": "hsl(210, 4%, 67%)",
+			},
+			fontFamily: {
+				fraunces: ["Fraunces", "serif"],
+				barlow: ["Barlow", "sans serif"],
+			},
+		},
+	},
+	plugins: [],
+};
